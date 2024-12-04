97c844ae440cba7bcdff6eeca4036c8e1f3119be
Pury-01 <puryokumu@gmail.com>
2024-12-04 14:39:18 +0300

    function that initializes the flask app

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""Initialize the app"""
+from flask import Flask
+
+
+def create_app():
+    """instance of flask application"""
+    app = Flask(__name__)
+    from .routes import bp as main_bp
+    app.register_blueprint(main_bp)
+    return app
