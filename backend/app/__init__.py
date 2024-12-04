948231b7b6c111323603de2878c01cee02acc489
Pury-01 <puryokumu@gmail.com>
2024-12-04 15:38:15 +0300

    imported blueprint in routes to flask application for accessibility

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-"""Initialize the app"""
+"""Initialize flask app"""
 from flask import Flask
+from .routes import bp as main_bp
 
 
 def create_app():
-    """instance of flask application"""
+    """factory function to initialize flask app, register
+    Blueprints, and set configurations.
+    Returns:
+        Flask app instance
+    """
     app = Flask(__name__)
-    from .routes import bp as main_bp
+    
     app.register_blueprint(main_bp)
+
     return app
