04efcf666ac166796921149fc8b2a73ab71e4283
Pury-01 <puryokumu@gmail.com>
2024-12-04 18:38:38 +0300

    implemented route for fetching recipes returning an empty list as placeholder for recipe data

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """home route"""
-from flask import Blueprint, send_from_directory
+from flask import Blueprint, send_from_directory, jsonify
 
 
 bp = Blueprint("main", __name__, static_folder='../frontend/build', static_url_path='/')
@@ -12,3 +12,10 @@ def Serve_react_app():
     """serve the React app's index.html for the root route.
     """
     return send_from_directory(bp.static_folder, 'index.html')
+
+@bp.route('/api/recipes', methods=['GET'])
+def get_recipes():
+    """endpoint to fetch recipes"""
+    recipes = []
+
+    return jsonify(recipes)
