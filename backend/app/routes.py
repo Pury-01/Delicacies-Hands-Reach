dc64bc6b95c10d1a9b3d9cd68b0bf19250f592cd
Pury-01 <puryokumu@gmail.com>
2024-12-12 16:12:38 +0300

    added the logic and API url for fetching recipes by ingredients

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
-"""home route"""
-from flask import Blueprint, send_from_directory, jsonify
+"""API endpoints"""
+from flask import Blueprint, send_from_directory, jsonify, request
+import requests
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
 
 
 bp = Blueprint("main", __name__, static_folder='../frontend/build', static_url_path='/')
@@ -15,7 +20,44 @@ def Serve_react_app():
 
 @bp.route('/api/recipes', methods=['GET'])
 def get_recipes():
-    """endpoint to fetch recipes"""
-    recipes = []
+    """endpoint to fetch recipes from Spoonacular API"""
+    # fetch the query parameter from the request
+    ingredients = request.args.get('ingredients', '')
+    
+    API_KEY = "SPOONACULAR_API_KEY"
+    find_recipe_url = "https://api.spoonacular.com/recipes/findByIngredients"
+    # recipe_steps_url = "https://api.spoonacular.com/recipes/{id}/analyzedInstructions"
 
-    return jsonify(recipes)
+    
+
+    # parameters to pass to Spoonacular API
+    params = {
+        "ingredients": ingredients,
+        "apiKey": API_KEY,
+        "number": 10
+    }
+    
+    try:
+        # send GET request to the Spoonacular API
+        response = requests.get(find_recipe_url, params=params)
+        # raise an exception if request fails
+        response.raise_for_status()
+
+        # parse the response
+        data = response.json()
+        print(data)
+
+        # get recipes from the response
+        recipes = [
+            {
+                "id": recipe.get('id'),
+                "name": recipe.get('title'),
+                "ingredients": [
+                    ingredient.get('name') for ingredient in recipe.get('usedIgredients', []) + recipe.get('missedIngredients', []) 
+                    ]
+            }
+            for recipe in data
+        ]
+        return jsonify(recipes)
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
