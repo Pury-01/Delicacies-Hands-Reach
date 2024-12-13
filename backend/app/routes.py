cbdd2f8601e46dd5417b380db4b712b362c55d5d
Pury-01 <puryokumu@gmail.com>
2024-12-13 17:21:08 +0300

    integrated the findByIngredients API with analyzedInstructions API from spoonacular asychnronously so that the recipe, ingredients and steps are returned with a single request

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """API endpoints"""
 from flask import Blueprint, send_from_directory, jsonify, request
-import requests
 import os
+import requests
+import asyncio
+import aiohttp
 from dotenv import load_dotenv
 from . import db
 
@@ -10,7 +12,7 @@ from . import db
 load_dotenv()
 
 
-bp = Blueprint("main", __name__, static_folder='../frontend/build', static_url_path='/')
+bp = Blueprint("main", __name__)
 
 
 # serve react build files
@@ -21,15 +23,13 @@ def Serve_react_app():
     return send_from_directory(bp.static_folder, 'index.html')
 
 @bp.route('/api/recipes', methods=['GET'])
-def get_recipes():
+async def get_recipes():
     """endpoint to fetch recipes from Spoonacular API"""
     # fetch the query parameter from the request
     ingredients = request.args.get('ingredients', '')
     
     API_KEY = os.getenv('SPOONACULAR_API_KEY')
     find_recipe_url = "https://api.spoonacular.com/recipes/findByIngredients"
-    # recipe_steps_url = "https://api.spoonacular.com/recipes/{id}/analyzedInstructions"
-
     
 
     # parameters to pass to Spoonacular API
@@ -39,32 +39,44 @@ def get_recipes():
         "number": 10
     }
     
-    try:
-        # send GET request to the Spoonacular API
-        response = requests.get(find_recipe_url, params=params)
-        # raise an exception if request fails
-        response.raise_for_status()
+    # empty list to stre the recipes
+    recipes = []
 
-        # parse the response
-        data = response.json()
-        print(data)
+    # perform http requests asyncronously
+    async with aiohttp.ClientSession() as session:
+        try:
+             # send GET request to the Spoonacular API asynchronously
+             async with session.get(find_recipe_url, params=params) as response:
+                # raise an exception if request fails
+                response.raise_for_status()
 
-        # get recipes from the response
-        recipes = [
-            {
-                "id": recipe.get('id'),
-                "name": recipe.get('title'),
-                "ingredients": [
-                    ingredient.get('name') for ingredient in recipe.get('usedIgredients', []) + recipe.get('missedIngredients', []) 
-                    ]
-            }
-            for recipe in data
-        ]
-        return jsonify(recipes)
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error": str(e)}), 500
+                # parse the response
+                data = await response.json()
 
+                # get recipes from the response
+                for recipe in data:
+                    # request to get the recipe steps from a 2nd endpoint API
+                    recipe_steps_url = f"https://api.spoonacular.com/recipes/{recipe['id']}/analyzedInstructions"
+                    async with session.get(recipe_steps_url, params={"apiKey": API_KEY}) as steps_response:
+                        steps_response.raise_for_status()
+                        steps_data = await steps_response.json()
 
+                        # add the steps to the recipe
+                        steps = steps_data[0]['steps'] if steps_data else []
+
+                    # store the recipe's data 
+                    recipes.append({
+                        "id": recipe.get('id'),
+                        "name": recipe.get('title'),
+                        "ingredients": [
+                            ingredient.get('name') for ingredient in recipe.get('usedIgredients', []) + recipe.get('missedIngredients', []) 
+                            ],
+                            "steps":  [step['step'] for step in steps]
+    })
+            
+        except requests.exceptions.RequestException as e:
+            return jsonify({"error": str(e)}), 500
+    return jsonify(recipes)        
 @bp.route("/api/save", methods=['POST'])
 def save_recipe():
     """Endpoint to save a recipe to the database."""
